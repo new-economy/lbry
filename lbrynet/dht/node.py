9a579fccab4d31100ed107eb9797a5f447c87738
Jack Robison <jackrobison@lbry.io>
2018-05-15 14:11:19 -0400

    remove OptimizedTreeRoutingTable, use TreeRoutingTable

--- lbrynet/dht/node.py
+++ lbrynet/dht/node.py
@@ -131,7 +131,7 @@ class Node(MockKademliaHelper):
 
         # Create k-buckets (for storing contacts)
         if routingTableClass is None:
-            self._routingTable = routingtable.OptimizedTreeRoutingTable(self.node_id, self.clock.seconds)
+            self._routingTable = routingtable.TreeRoutingTable(self.node_id, self.clock.seconds)
         else:
             self._routingTable = routingTableClass(self.node_id, self.clock.seconds)
 
